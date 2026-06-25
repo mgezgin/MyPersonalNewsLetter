bb069f7435c7bff1cdcad65257bead36d5262ec1
mgezgin <gezginmuslum951@gmail.com>
2026-06-25 16:26:40 +0200

    fix navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -14,7 +14,7 @@ export default function Navigation() {
             <div className="container mx-auto px-4 py-4">
                 <div className="flex items-center justify-between">
                     <Link href="/" className="text-2xl font-bold">
-                        My Newsletter
+                        Muslum Gezgin
                     </Link>
 
                     {/* Desktop Navigation */}
